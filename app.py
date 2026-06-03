2d6d847ff9128dddb147874358595d96d18bd334
korawitkl54-del <korawit.kl54@gmail.com>
2026-06-03 20:16:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ def save_to_sheets(source, entry_time):
     sheet.append_row([source, entry_time])
 
 # --- หน้าจอหลัก ---
-st.set_page_config(page_title="Boss Mel Performance Lab", layout="wide")
-st.title("⚡ Boss Mel Performance Lab")
+st.set_page_config(page_title="SpeedCalc", layout="wide")
+st.title("⚡SpeedCalc")
 
 if 'user_info' not in st.session_state:
     with st.form("user_form"):
-        source = st.text_input("คุณมาจากที่ไหนครับ?")
+        source = st.text_input("ระบุโรงเรียน/หน่วยงาน")
         submit = st.form_submit_button("เข้าสู่ระบบ")
         if submit and source:
             t = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
