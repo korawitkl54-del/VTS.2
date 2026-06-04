af69fce5a5b7332a4fd0024db9231cbe80b01df1
korawitkl54-del <korawit.kl54@gmail.com>
2026-06-04 15:32:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ with st.sidebar:
     st.markdown("---")
     st.write("ระบบคำนวณเชิงประสิทธิภาพสูง")
 
-st.title("⚡ Telescopic Lab")
+st.title("⚡(Telescoping Cubic)จากคณิตศาสตร์บริสุทธิ์สู่อัลกอริทึมการประมวลผลความเร็วสูง
+")
 
 # ลิงก์ Web App ของอ้าย
 WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyC87oA6lqQaxWUfo8y5OtImplEP2552O1C-Tj2zTctw1cyeMC1Tm7F7M2Ag9FkN3lR/exec"
