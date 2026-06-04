532bb00120c87942e6923ef751674c723193cdc4
korawitkl54-del <korawit.kl54@gmail.com>
2026-06-04 15:29:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 from datetime import datetime
 
 # --- ตั้งค่าหน้าเว็บ ---
-st.set_page_config(page_title="เทเลสโคปิกกำลังสาม:จากคณิตศาสตร์บริสุทธิ์สู่อัลกอริทึมการประมวลผลความเร็วสูง", layout="wide")
+st.set_page_config(page_title="เทเลสโคปิกกำลังสามจากคณิตศาสตร์บริสุทธิ์สู่อัลกอริทึมการประมวลผลความเร็วสูง", layout="wide")
 
 # --- ใส่ตราโรงเรียนที่ Sidebar ---
 with st.sidebar:
